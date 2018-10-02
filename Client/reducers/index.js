2a2ee658542a2f8736507e0c56667e25716876c3
Richard Onengiye <31253707+klevamane@users.noreply.github.com>
2018-10-02 17:21:01 +0100

    Revert "#160929393 setup redux"

--- Client/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-export default combineReducers({
-     // When used in any component anything from auth reducer will
-    // be used as this.props.auth as opposed to this.props.authReducer
-
-    // add reducers here
-});
